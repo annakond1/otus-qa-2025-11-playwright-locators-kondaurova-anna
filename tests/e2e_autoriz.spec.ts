0ce54b892d0c2c063ea55ca909a6fda262b5b22c
Анна Кондаурова <anyutakond@gmail.com>
2026-04-05 12:48:31 +0300

    e2e_autoriz.spec.ts

--- tests/e2e_autoriz.spec.ts
+++ tests/e2e_autoriz.spec.ts
@@ -5,7 +5,11 @@ const selectors = {
     username: 'input#username',
     password: 'input[name="password"]',
     loginBut: 'button.radius',
-    logout: 'a[href="/logout"]'
+    logout: 'a[href="/logout"]',
+    errorHint: 'a[href="#"]',
+    gitHubBut: 'img[alt="Fork me on GitHub"]',
+    gitHubPage: 'a[href="/saucelabs/the-internet/commits?author=diemol"]',
+    //elementalSelenium: 'a[href="http://elementalselenium.com/"]'
 };
 
 // креды для авторизации
@@ -24,65 +28,131 @@ test.describe('e2e_autorize', () => {
         });
 });
 
-test('Страница авторизации открыта', async ({ page }) => {
-//const title = await page.getByRole('heading', { name: 'Login Page' })
-//await expect (page.getByRole('heading', { name: 'Login Page' })).toBeVisible()})
+test('Отображение страницы авторизации', async ({ page }) => {
+        //const title = await page.getByRole('heading', { name: 'Login Page' })
+        //await expect (page.getByRole('heading', { name: 'Login Page' })).toBeVisible()})
 
-// Проверяем, что поле username видимо на странице
+        // Проверяем, что поле username видимо на странице
         //await expect(page.locator(selectors.username)).toBeVisible();
         await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
 });
 
-test('Проверим успешную авторизацию', async ({ page }) => {
-    // 1. Ждем загрузки поля username
+test('Проверка успешной авторизации', async ({ page }) => {
+        // 1. Ждем загрузки поля username
         await page.waitForSelector(selectors.username, { 
             state: 'visible', 
             timeout: 50000 
         });
-    
-    // 2. Заполняем username
-    await page.locator(selectors.username).fill(credentials.login);
-    //await page.locator(selectors.password).click();
-
-    // 3. Заполняем password 
-    //await page.locator(selectors.password).press('ControlOrMeta+м');
-    await page.locator(selectors.password).fill(credentials.password);
-    // 4. Нажимаем кнопку Login
-    await page.locator(selectors.loginBut).click();
-    // 5. Ждем загрузки после авторизации
-    await page.waitForLoadState('networkidle');
-    // 6. Проверяем страницу акворизованного аккаунта
-     await expect(page.getByText('Welcome to the Secure Area. When you are done click logout below.')).toBeVisible();
-    //expect(title).toBe('The Internet');
-    //await expect(page.getByText('Secure Area')).toBeVisible();
+        // 2. Заполняем username
+        await page.locator(selectors.username).fill(credentials.login);
+        //await page.locator(selectors.password).click();
+        // 3. Заполняем password 
+        //await page.locator(selectors.password).press('ControlOrMeta+м');
+        await page.locator(selectors.password).fill(credentials.password);
+       // 4. Нажимаем кнопку Login
+       await page.locator(selectors.loginBut).click();
+       // 5. Ждем загрузки после авторизации
+       await page.waitForLoadState('networkidle');
+       // 6. Проверяем страницу акворизованного аккаунта
+       await expect(page.getByText('Welcome to the Secure Area. When you are done click logout below.')).toBeVisible();
+       //expect(title).toBe('The Internet');
+       //await expect(page.getByText('Secure Area')).toBeVisible();
 });
 
-    test('Произведём успешную разавторизацию', async ({ page}) => {
+test('Произверка успешной разавторизации', async ({ page}) => {
+      // 1. Ждем загрузки поля username
+      await page.waitForSelector(selectors.username, { 
+        state: 'visible', 
+        timeout: 50000 
+      });
+      // 2. Заполняем username
+      await page.locator(selectors.username).fill(credentials.login);
+      //await page.locator(selectors.password).click();
+      // 3. Заполняем password 
+      await page.locator(selectors.password).fill(credentials.password);
+      // 4. Нажимаем кнопку Login
+      await page.locator(selectors.loginBut).click();
+      // 5. Ждем загрузки после авторизации
+      await page.waitForLoadState('networkidle');
+      // 6. Проверяем страницу акворизованного аккаунта
+      await expect(page.getByText('Welcome to the Secure Area. When you are done click logout below.')).toBeVisible();
+      // 7. Нажимаем на кнопку Logout
+      await page.locator(selectors.logout).click();  
+      // 8. Ждем загрузки после авторизации
+      await page.waitForLoadState('networkidle');
+      // 9. Проверяем страницу авторизации
+      await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible(); 
+});
+
+test('Проверка ошибочной авторизации при пустых полях ввода Username и Password', async ({ page }) => {
+      // 1. Ждем загрузки поля username
+      await page.waitForSelector(selectors.username, { 
+      state: 'visible', 
+      timeout: 50000 
+      });
+      // 2. Нажимаем кнопку Login
+      await page.locator(selectors.loginBut).click();
+      // 3. Ждем загрузки после авторизации
+      await page.waitForLoadState('networkidle');
+      // 4. Проверяем страницу акворизованного аккаунта
+      await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
+});
+
+test('Проверка ошибочной авторизации при некорректных данных в полях Username и Password', async ({ page }) => {
+      // 1. Ждем загрузки поля username
+      await page.waitForSelector(selectors.username, { 
+         state: 'visible', 
+         timeout: 50000 
+      });
+      // 2. Заполняем username
+      await page.locator(selectors.username).fill(credentials.password);
+      //await page.locator(selectors.password).click();
+      // 3. Заполняем password 
+      await page.locator(selectors.password).fill(credentials.login);
+      // 4. Нажимаем кнопку Login
+      await page.locator(selectors.loginBut).click();
+      // 5. Ждем загрузки после авторизации
+      await page.waitForLoadState('networkidle');
+      // 6. Проверяем страницу авторизованного аккаунта
+      await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
+});
+
+test('Проверка ошибки авторизации с некорреткным паролем', async ({ page }) => {
+        // 1. Ждем загрузки поля username
+        await page.waitForSelector(selectors.username, { 
+            state: 'visible', 
+            timeout: 50000 
+        });
+        // 2. Заполняем username
+        await page.locator(selectors.username).fill(credentials.login);
+        // 3. Заполняем password 
+        await page.locator(selectors.password).fill(credentials.login);
+       // 4. Нажимаем кнопку Login
+       await page.locator(selectors.loginBut).click();
+       // 5. Ждем загрузки после авторизации
+       await page.waitForLoadState('networkidle');
+       // 6. Проверяем страницу авторизации
+      await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
+});
+
+test('Проверка ошибки авторизации с пустым паролем', async ({ page }) => {
     // 1. Ждем загрузки поля username
         await page.waitForSelector(selectors.username, { 
             state: 'visible', 
             timeout: 50000 
         });
-    // 2. Заполняем username
-    await page.locator(selectors.username).fill(credentials.login);
-    //await page.locator(selectors.password).click();
-    // 3. Заполняем password 
-    await page.locator(selectors.password).fill(credentials.password);
-    // 4. Нажимаем кнопку Login
-    await page.locator(selectors.loginBut).click();
-    // 5. Ждем загрузки после авторизации
-    await page.waitForLoadState('networkidle');
-    // 6. Проверяем страницу акворизованного аккаунта
-    await expect(page.getByText('Welcome to the Secure Area. When you are done click logout below.')).toBeVisible();
-    // 7. Нажимаем на кнопку Logout
-    await page.locator(selectors.logout).click();  
-    // 8. Ждем загрузки после авторизации
-    await page.waitForLoadState('networkidle');
-    // 9. Проверяем страницу авторизации
-    await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible(); 
-    });
+        // 2. Заполняем username
+        await page.locator(selectors.username).fill(credentials.login);
+       // 3. Нажимаем кнопку Login
+       await page.locator(selectors.loginBut).click();
+       // 4. Ждем загрузки после авторизации
+       await page.waitForLoadState('networkidle');
+       // 5. Проверяем страницу авторизации
+      await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
 
-    test('Ошибочная авторизация при пустых полях ввода Username и Password', async ({ page }) => {
+});
+
+test('Проверка подсказки на странице авторизации при ошибке авторизации (появление и закрытие)', async ({ page}) => {
     // 1. Ждем загрузки поля username
         await page.waitForSelector(selectors.username, { 
             state: 'visible', 
@@ -94,25 +164,64 @@ test('Проверим успешную авторизацию', async ({ page }
     await page.waitForLoadState('networkidle');
     // 4. Проверяем страницу акворизованного аккаунта
     await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
-    });
+    await expect(page.getByText('Your username is invalid!')).toBeVisible();
+    await page.locator(selectors.errorHint).click();
+    await page.waitForTimeout(500);
+    await expect(page.getByText('This is where you can log into the secure area. Enter ')).not.toBeHidden();
+});
 
-    test('Ошибочная авторизация при некорректных данных Username и Password', async ({ page }) => {
+test('Проверка успешной авторизации при отображении подсказки об ошибке предыдущей авторизации', async ({ page }) => {
     // 1. Ждем загрузки поля username
         await page.waitForSelector(selectors.username, { 
             state: 'visible', 
             timeout: 50000 
         });
-    // 2. Заполняем username
-    await page.locator(selectors.username).fill(credentials.password);
-    //await page.locator(selectors.password).click();
-    // 3. Заполняем password 
-    await page.locator(selectors.password).fill(credentials.login);
-    // 4. Нажимаем кнопку Login
+    // 2. Нажимаем кнопку Login
     await page.locator(selectors.loginBut).click();
-    // 5. Ждем загрузки после авторизации
+    // 3. Ждем загрузки после авторизации
     await page.waitForLoadState('networkidle');
-    // 6. Проверяем страницу акворизованного аккаунта
+    // 4. Проверяем страницу акворизованного аккаунта
     await expect(page.getByText('This is where you can log into the secure area. Enter ')).toBeVisible();
-    });
+    // 5. Заполняем username
+    await page.locator(selectors.username).fill(credentials.login);
+    // 6. Заполняем password 
+    await page.locator(selectors.password).fill(credentials.password);
+    // 7. Нажимаем кнопку Login
+    await page.locator(selectors.loginBut).click();
+    // 8. Ждем загрузки посля авторизации
+    await page.waitForLoadState('networkidle');
+    // 9. Проверяем страницу авторизованного аккаунта
+     await expect(page.getByText('Welcome to the Secure Area. When you are done click logout below.')).toBeVisible();      
 });
+
+test('Проверка функции просмотра введённого пароля', async ({ page }) => {
+     // 1. Ждем загрузки поля username
+        await page.waitForSelector(selectors.username, { 
+            state: 'visible', 
+            timeout: 50000 
+        });
+    // 2. Заполняем password 
+    await page.locator(selectors.password).fill(credentials.password);
+    // 3. Кликаем в правую часть поля password
+    await page.locator(selectors.password).click({ position: { x: 465, y: 15} });
+    expect(page.locator(selectors.password)).toHaveValue('SuperSecretPassword!');
+});
+
+test('Проверка перехода в GitHub со страницы разавторизованного пользователя', async ({ page }) => {
+    await page.locator(selectors.gitHubBut).click();
+    await expect(page.locator(selectors.gitHubPage)).toBeVisible();   
+});
+
+test ('Проверка перехода по ссылке Elemental Selenium', async ({ page, context }) => {
+    // Ждём открытия новой вкладки
+    const [elSel] = await Promise.all([
+      context.waitForEvent('page'),  // Ожидаем новую вкладку
+      page.click('a[href="http://elementalselenium.com/"]') // Клик, который открывает вкладку
+    ]);
+    await page.waitForLoadState(); // Ждем загрузки после авторизации
+    await expect(elSel).toHaveURL('https://elementalselenium.com');
+    await expect(page.getByText('Elemental Selenium')).toBeVisible(); // проверка новой вкладки
+});
+});
+
 
